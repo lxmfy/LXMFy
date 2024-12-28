180fe70fe78f12f9facf80301e8512920d707200
Ivan <ivan@quad4.io>
2024-12-28 02:59:25 -0600

    bump v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LXMFy",
-    version="0.1.0",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=["RNS", "LXMF"],
     author="Sudo-Ivan",
@@ -10,7 +10,7 @@ setup(
     description="A Discord.py-like bot framework for LXMF",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/Sudo-Ivan/LXMFy",
+    url="https://github.com/lxmfy/LXMFy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
