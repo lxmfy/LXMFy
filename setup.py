50c343e5ced573b7c2c62cc59020df1a7de0aa7b
Ivan <ivan@quad4.io>
2025-04-06 23:22:29 -0500

    v0.6.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="LXMFy",
-    version="0.6.4",
+    version="0.6.5",
     packages=find_packages(),
     install_requires=["RNS", "LXMF"],
     author="Sudo-Ivan",
