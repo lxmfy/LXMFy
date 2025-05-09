5e74a1ad483f02daae5ab1340f6387539412f1ab
Ivan <Sudo-Ivan@users.noreply.github.com>
2025-05-09 17:37:43 -0500

    bump to 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="lxmfy",
-    version="0.6.9",
+    version="0.7.0",
     packages=find_packages(),
     install_requires=["RNS", "LXMF"],
     author="Quad4",
